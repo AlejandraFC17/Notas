302897a52f999a0513f3cd755244384393566847
ALEJANDRA <Alejandrafc9908@gmail.com>
2023-03-28 21:07:34 -0500

    GradeBook

---
+++ project/Notas/src/GradeBook/GradeBook.java
@@ -0,0 +1,8 @@
+package GradeBook;
+
+public class GradeBook {
+    public void displayMessage()
+    {
+        System.out.println("Bienvenido al sistema de Notas");
+    }
+}
---
+++ project/Notas/src/GradeBook/GradeBookTest.java
@@ -0,0 +1,10 @@
+package GradeBook;
+
+public class GradeBookTest {
+    public static void main (string[] args) {
+        //TODO Auto-generated method stub
+        GradeBook myGradeBook = new GradeBook();
+
+                myGradeBook.displayMessage();
+    }
+}
